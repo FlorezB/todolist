c58227a49f81b14c597f43816da6ee9e3fe50322
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-08 15:53:40 +0200

    function swapCheckColor done

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
 // variables and constantes
 
-let checkedNumber = 0
+let checkedNumber = 1
 let taskNumber = 1
 let tasks = []
+let test
 const checkedImg = document.getElementById(`checkedImg${taskNumber}`) // a voir
 const taskHistory = document.getElementById("task-history")
 const formTitle = document.getElementById("form-title")
 const formDescription = document.getElementById("form-description")
 const priorityOfTask = document.getElementById("priority-range")
+
 //
 
-// déclaration de classe
-// class Task {
-//     constructor(id, title, description, priorityValue) {
-//         this.id = id
-//         this.title = title
-//         this.description = description
-//         this.priority = priorityValue
-//         this.status = "to do"
-//       }
-// }
 
-// fonction pour réset
+
+// fonction pour reset
 const initialize = () => {
-    checkedNumber = 0
+    checkedNumber = 1
     taskNumber = 1
     tasks = []
     taskHistory.innerHTML = ``
 }
 
-const swapCheckColor = () => {
-    checkedNumber ++
-    checkedImg2.setAttribute("src", `./img/coche${checkedNumber}.png`)
+// fonction pour changer le status des taches
+const swapCheckColor = (checkedImgTaskID) => {
+    test = document.getElementById(`checkedImg${checkedImgTaskID}`)
+    test.setAttribute("src", `./img/coche${checkedNumber}.png`)
     if (checkedNumber === 2) {
         checkedNumber = -1
     }
+    checkedNumber ++
 }
 
+// fonction qui récupère les données du formulaire
 const onTaskSubmit= () => {
     tasks.push({id: taskNumber, title: formTitle.value, description: formDescription.value, priority: priorityOfTask.value})
     writeTask()
-
-    // console.log(formTitle.value)
-    // console.log(formDescription.value)
-    // console.log(priorityOfTask.value)
 }
+
+//fonction qui écrit les taches en html
 const writeTask = () => {
     taskHistory.innerHTML = ``
     tasks.forEach((task) => {
     taskHistory.innerHTML = taskHistory.innerHTML + `
     <div class="flex width_100p100 flex-column gap5 padding-top_20">
         <div class="flex justifyContent-spaceBetween alignItem-flexEnd padding-right_30">
-            <button onclick="swapCheckColor()" class="border-none background-none" id="checkedImgButton${task.id}"><img id="checkedImg${task.id}" src="./img/coche0.png" alt="image de coche dans un cercle" height="40px" width="40px"/></button>
+            <button onclick="swapCheckColor(${task.id})" class="border-none background-none" id="checkedImgButton${task.id}"><img id="checkedImg${task.id}" src="./img/coche0.png" alt="image de coche dans un cercle" height="40px" width="40px"/></button>
             <h2 class="padding-left_30">Task #${task.id} : ${task.title}</h2>
                 <div class="flex">
                     <img id="editButton${task.id}" src="./img/editer.png" alt="image de crayon" height="40px" width="40px">
