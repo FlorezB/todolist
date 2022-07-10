079b8462203603f9b1dc433fb776cdea1836778d
Caroline <alyseflorez@hotmail.fr>
2022-07-10 19:15:25 +0200

    priority

--- index.js
+++ index.js
@@ -3,65 +3,49 @@
 let checkedNumber = 0
 let taskNumber = 1
 let tasks = []
-const checkedImg = document.getElementById(`checkedImg${taskNumber}`) // a voir
+const checkedImg = document.getElementById(`checkedImg${taskNumber}`)
 const taskHistory = document.getElementById("task-history")
 const formTitle = document.getElementById("form-title")
 const formDescription = document.getElementById("form-description")
 const priorityOfTask = document.getElementById("priority-range")
 //
 
-// déclaration de classe
-// class Task {
-//     constructor(id, title, description, priorityValue) {
-//         this.id = id
-//         this.title = title
-//         this.description = description
-//         this.priority = priorityValue
-//         this.status = "to do"
-//       }
-// }
-
-// fonction pour réset
 const initialize = () => {
     checkedNumber = 0
     taskNumber = 1
     tasks = []
-    taskHistory.innerHTML = ``
+
 }
 
 const swapCheckColor = () => {
     checkedNumber ++
-    checkedImg2.setAttribute("src", `./img/coche${checkedNumber}.png`)
+    checkedImg.setAttribute("src", `./img/coche${checkedNumber}.png`)
     if (checkedNumber === 2) {
         checkedNumber = -1
     }
 }
 
 const onTaskSubmit= () => {
-    tasks.push({id: taskNumber, title: formTitle.value, description: formDescription.value, priority: priorityOfTask.value})
-    writeTask()
+    taskHistory.innerHTML = taskHistory.innerHTML + `
+    <div class="flex width_100p100 flex-column gap5 padding-top_20">
+        <div class="flex justifyContent-spaceBetween alignItem-flexEnd padding-right_30">
+            <button onclick="swapCheckColor()" class="border-none background-none"><img id="checkedImg${taskNumber}" src="./img/coche0.png" alt="image de coche dans un cercle" height="40px" width="40px"/></button>
+            <h2 class="padding-left_30">Task #${taskNumber} : ${formTitle.value}</h2>
+                <div class="flex">
+                    <img src="./img/editer.png" alt="image de crayon" height="40px" width="40px">
+                    <img src="./img/corbeilleRouge.png" alt="image de corbeille" height="40px" width="40px">
+                </div>
+        </div>
+        <div class="border">   <!-- ligne-->   </div>
+        <p>${formDescription.value}</p>
+    </div>
+    `
+    taskNumber ++
+
+
+
 
     // console.log(formTitle.value)
     // console.log(formDescription.value)
     // console.log(priorityOfTask.value)
-}
-const writeTask = () => {
-    taskHistory.innerHTML = ``
-    tasks.forEach((task) => {
-    taskHistory.innerHTML = taskHistory.innerHTML + `
-    <div class="flex width_100p100 flex-column gap5 padding-top_20">
-        <div class="flex justifyContent-spaceBetween alignItem-flexEnd padding-right_30">
-            <button onclick="swapCheckColor()" class="border-none background-none" id="checkedImgButton${task.id}"><img id="checkedImg${task.id}" src="./img/coche0.png" alt="image de coche dans un cercle" height="40px" width="40px"/></button>
-            <h2 class="padding-left_30">Task #${task.id} : ${task.title}</h2>
-                <div class="flex">
-                    <img id="editButton${task.id}" src="./img/editer.png" alt="image de crayon" height="40px" width="40px">
-                    <img id="supprButton${task.id}" src="./img/corbeilleRouge.png" alt="image de corbeille" height="40px" width="40px">
-                </div>
-        </div>
-        <div class="border">   <!-- ligne-->   </div>
-        <p>${task.description}</p>
-    </div>
-    `
-    })
-    taskNumber ++
 }
